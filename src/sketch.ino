f354a96e07161bd23b59d54e0736e068d31a4796
Tyler Benziger <tabenziger@gmail.com>
2014-12-05 10:06:03 -0800

    Simple set

--- src/sketch.ino
+++ src/sketch.ino
@@ -4,18 +4,38 @@ int sensors[] = {9, 8, 5, 3, 7, 6};
 Car* car = new Car(10, 11, 12, sensors);
 
 long friction = 1.0;
-int forward = 1000 * friction;
-int stop = 250 * friction;
-int turn = 300 * friction;
+int forward = 550;
+int stopDelay = 350;
+int turn = 340;
+boolean go = true;
 
 void setup() {
 }
 
 void loop() {
+    moveForward();
+    turnRight();
+}
+
+void moveForward() {
     car->moveForward();
     delay(forward);
-    car->stop();
-    delay(stop);
+    stop();
+}
+
+void turnRight() {
     car->turnRight();
     delay(turn);
+    stop();
+}
+
+void turnLeft() {
+    car->turnLeft();
+    delay(turn);
+    stop();
+}
+
+void stop() {
+    car->stop();
+    delay(stopDelay);
 }

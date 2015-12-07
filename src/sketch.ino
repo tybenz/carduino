b1072e85c82832a9404da22c71b9491be8ff7036
Tyler Benziger <tabenziger@gmail.com>
2015-12-07 13:00:19 -0800

    Calibrate for conference room

--- src/sketch.ino
+++ src/sketch.ino
@@ -7,10 +7,12 @@
 int sensors[] = {9, 8, 5, 3, 7, 6};
 Car* car = new Car(10, 11, 12, sensors);
 
+
+
 long friction = 1.0;
-int forward = 470;
+int forward = 640;
 int stopDelay = 400;
-int turn = 555;
+int turn = 600;
 boolean done = false;
 
 void setup() {
